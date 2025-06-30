07abed3d02417cbed79c22a7a5e5bf56ffb8922b
Gengo-bit <paulcorsino28@gmail.com>
2025-07-01 00:22:26 +0800

    finish modal for mobile mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,122 @@ import { useState } from "react";
 import logo from "../assets/GenSupply.png";
 import { useEffectHooks } from "../hooks/useEffectHooks";
 import MenuIcon from "@mui/icons-material/Menu";
-import MenuOpenIcon from "@mui/icons-material/MenuOpen";
-import { NavigationHooks } from "../hooks/NavigationHooks";
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function Header() {
   const screenWidth = useEffectHooks();
-  const { navLogin, navRegister } = NavigationHooks();
 
   const [hamburgerClicked, setHamburgerClicked] = useState(false);
   const handleHamburgerClick = () => {
     setHamburgerClicked(!hamburgerClicked);
   };
   return (
-    <header className="w-screen h-[100px] fixed flex flex-row ">
-      <div className="flex w-[50%] bg-slate-500 items-center justify-start ">
+    <header className="w-screen h-[80px] fixed flex flex-row border-b-zinc-300 border-b border-solid">
+      {hamburgerClicked === false ? (
+        <nav className="absolute mt-[80px] h-screen w-screen flex bg-red-50 flex-col items-center">
+          <div className="h-[60%] w-screen items-center flex flex-col">
+            <div className="border-y-zinc-400 border-b border-solid w-[90%] pt-3 pb-3 flex group">
+              <div className="w-[50%] flex ">
+                {" "}
+                <a
+                  href="#home"
+                  className="no-underline text-[1.5rem] text-zinc-800 font-semibold group-hover:text-red-700 ml-[10px]"
+                >
+                  Home
+                </a>
+              </div>
+              <div className="w-[50%] flex justify-end items-center">
+                <ArrowForwardIosIcon fontSize="medium" className="mr-3" />
+              </div>
+            </div>
+            <div className="border-y-zinc-400 border-b border-solid w-[90%] pt-3 pb-3 flex group">
+              <div className="w-[50%] flex">
+                {" "}
+                <a
+                  href="#about"
+                  className="no-underline text-[1.5rem] text-zinc-800 font-semibold group-hover:text-red-700 ml-[10px]"
+                >
+                  About
+                </a>
+              </div>
+              <div className="w-[50%] flex justify-end items-center">
+                <ArrowForwardIosIcon fontSize="medium" className="mr-3" />
+              </div>
+            </div>
+
+            <div className="border-y-zinc-400 border-b border-solid w-[90%] pt-3 pb-3 flex group">
+              <div className="w-[50%] flex">
+                {" "}
+                <a
+                  href="#pricing"
+                  className="no-underline text-[1.5rem] text-zinc-800 font-semibold group-hover:text-red-700 ml-[10px]"
+                >
+                  Pricing
+                </a>
+              </div>
+              <div className="w-[50%] flex justify-end items-center">
+                <ArrowForwardIosIcon fontSize="medium" className="mr-3" />
+              </div>
+            </div>
+            <div className="border-y-zinc-400 border-b border-solid w-[90%] pt-3 pb-3 flex group">
+              <div className="w-[50%] flex">
+                {" "}
+                <a
+                  href="#demo"
+                  className="no-underline text-[1.5rem] text-zinc-800 font-semibold group-hover:text-red-700 ml-[10px]"
+                >
+                  Demo
+                </a>
+              </div>
+              <div className="w-[50%] flex justify-end items-center">
+                <ArrowForwardIosIcon fontSize="medium" className="mr-3" />
+              </div>
+            </div>
+          </div>
+          <div className="h-[40%] w-screen flex flex-col items-center">
+            <a
+              href="/register"
+              className="no-underline text-[1.2rem] font-semibold pl-10 pr-10 pt-2 pb-2 rounded-lg w-[90%] flex justify-center bg-red-700 text-[white] hover:text-orange-100 hover:bg-red-600 ml-[10px]"
+            >
+              Get Started
+            </a>
+            <a
+              href="/login"
+              className="no-underline text-[1.2rem] text-red-700 pl-10 pr-10 pt-2 pb-2 font-semibold w-[90%] flex justify-center ml-[10px] mt-4 border-[1px] rounded-[10px] border-solid border-red-700 hover:bg-red-700 hover:text-red-50"
+            >
+              Log In
+            </a>
+          </div>
+        </nav>
+      ) : (
+        <></>
+      )}
+      <div className="flex w-[50%] items-center justify-start ">
         {screenWidth > 600 ? (
           <>
             <img
               src={logo}
               alt="GenSupply Logo"
-              className="h-[60px] w-[60px]"
+              className="h-[60px] w-[60px] ml-[30px]"
             />
             <nav>
-              <a href="" className="no-underline text-[2rem] text-[white]">
+              <a
+                href="#home"
+                className="no-underline text-[1.5rem] text-zinc-800 ml-[30px] font-semibold hover:text-red-700"
+              >
                 Home
               </a>
-              <a href="" className="no-underline text-[2rem] text-[white]">
+              <a
+                href="#about"
+                className="no-underline text-[1.5rem] text-zinc-800 ml-[30px] font-semibold hover:text-red-700"
+              >
                 About
               </a>
-              <a href="" className="no-underline text-[2rem] text-[white]">
+              <a
+                href="#pricing"
+                className="no-underline text-[1.5rem] text-zinc-800 ml-[30px] font-semibold hover:text-red-700"
+              >
                 Pricing
               </a>
             </nav>
@@ -40,38 +127,51 @@ function Header() {
             <img
               src={logo}
               alt="GenSupply Logo"
-              style={{ margin: "0 0 0 25px" }}
+              className="h-[50px] w-[50px] ml-[30px]"
             />
           </>
         )}
       </div>
-      <div className="flex w-[50%] flex-row justify-end bg-zinc-700">
+      <div className="flex w-[50%] items-center justify-end">
         {screenWidth > 600 ? (
-          <nav>
-            <h2>
-              <span className="header-text">Demo</span>
-            </h2>
-            <h2>
-              <span onClick={navLogin} className="header-text">
-                Login
-              </span>
-            </h2>
-            <h2 onClick={navRegister}>
-              <span className="header-margin header-text cancel-margin">
-                Create Account
-              </span>
-            </h2>
-          </nav>
+          <>
+            <a
+              href="/demo"
+              className="no-underline text-[1.5rem] text-zinc-800 mr-[30px] font-semibold hover:text-red-700"
+            >
+              Demo
+            </a>
+            <a
+              href="/login"
+              className="no-underline text-[1.5rem] text-zinc-800 mr-[30px] font-semibold hover:text-red-700"
+            >
+              Login
+            </a>
+            <a
+              href="/register"
+              className="no-underline text-[1.5rem] text-zinc-800 mr-[30px] font-semibold hover:text-red-700 "
+            >
+              Create Account
+            </a>
+          </>
         ) : (
           <>
             {hamburgerClicked === true ? (
               <>
                 {" "}
-                <MenuIcon onClick={handleHamburgerClick} />
+                <MenuIcon
+                  onClick={handleHamburgerClick}
+                  fontSize="large"
+                  className="mr-[30px]"
+                />
               </>
             ) : (
               <>
-                <MenuOpenIcon onClick={handleHamburgerClick} />
+                <CloseIcon
+                  onClick={handleHamburgerClick}
+                  fontSize="large"
+                  className="mr-[30px]"
+                />
               </>
             )}
           </>
